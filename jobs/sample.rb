77c40d64f4f0979b997b49bbe6871ad439c26629
Daniel Hollands <daniel@limeblast.co.uk>
2014-09-08 18:15:46 +0000

    dashing init

---
+++ jobs/sample.rb
@@ -0,0 +1,16 @@
+SCHEDULER.every '2s' do
+  titles = [
+  	'What an awesome title, just superb',
+  	'This is also a pretty good title, by all accounts',
+  	'And this one, lets not forget this one'
+  ]
+  
+  texts = [
+  	'This is the first element of the array',
+  	'This is the second element of the array',
+  	'This is the third element of the array',
+  ]
+  
+  send_event('welcome', { title: titles.sample, text: texts.sample })
+  
+end
